f70d101dd6ed817346fdf93967444fc168936815
Chaste Djaziri <habimanahirwa@gmail.com>
2025-12-09 14:02:56 +0200

    about page start making

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero/Hero";
 import Stats from "./components/stats/Stats";
+import About from "./components/about/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Stats />
-      </main>
+      <About />
+    </main>
   );
 }
