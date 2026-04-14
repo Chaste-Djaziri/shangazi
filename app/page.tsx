fce4cedda148af262053f7d0d584759d833e6b72
Chaste Djaziri <habimanahirwa@gmail.com>
2026-04-14 15:55:11 +0200

    rearange the home page

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
     <main>
       <Hero />
       <About />
-      <SeoHighlights />
       <Stats />
+      <SeoHighlights />
       <Topics />
       <Featured />
       <Contact />
